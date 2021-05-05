7b3dd567dd96185965c2f9f5958e9dd6d8d226ff
SanneKerck <81096871+SanneKerck@users.noreply.github.com>
2021-05-05 22:38:40 +0200

    Update Hist.js

--- Hist.js
+++ Hist.js
@@ -1,6 +1,6 @@
          
         // set the dimensions and margins of the graph
-        var margin = {top: 20, right: 30, bottom: 160, left: 70},
+        var margin = {top: 30, right: 30, bottom: 160, left: 70},
         width = 800 - margin.left - margin.right,
         height = 800 - margin.top - margin.bottom;
     
